6bb486a1039f1df249c182bb9e85574f40855656
cabecomatheus-coder <cabecomatheus@gmail.com>
2026-08-28 18:43:11 -0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Lista de ativos da B3
-TICKERS = ["PETR4", "VALE3", "ITUB4", "BBDC4", "ABEV3", "BBAS3", "MGLU3", "WEGE3"]
-
-# Configuração de abas/páginas
-PAGES = ["Dashboard", "Gráficos", "Estratégia de Opções"]
